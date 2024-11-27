aaa96feab9e0747db6083cd317461b0b49e38dc6
Ricardo Sbalqueiro <Ricardosbalqueiro@gmail.com>
2024-11-27 03:02:38 -0300

    buildconfig

--- app/src/main/java/br/edu/up/rgm34295071/util/AuthInitializer.kt
+++ app/src/main/java/br/edu/up/rgm34295071/util/AuthInitializer.kt
@@ -3,9 +3,9 @@ package br.edu.up.rgm34295071.util
 import android.content.Context
 import androidx.startup.Initializer
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.BuildConfig
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import br.edu.up.rgm34295071.BuildConfig
 
 class AuthInitializer : Initializer<FirebaseAuth> {
     // The host '10.0.2.2' is a special IP address to let the
